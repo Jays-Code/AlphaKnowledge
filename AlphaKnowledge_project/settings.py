0d6fba424d118c7f83bbb811330879914416020f
Jays-Code <dfbfbc@gmail.com>
2019-06-07 11:50:45 -0400

    edited AK to AK_app

--- AlphaKnowledge_project/settings.py
+++ AlphaKnowledge_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'AlphaKnowledge.urls'
+ROOT_URLCONF = 'AlphaKnowledge_app.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'AlphaKnowledge.wsgi.application'
+WSGI_APPLICATION = 'AlphaKnowledge_app.wsgi.application'
 
 
 # Database
