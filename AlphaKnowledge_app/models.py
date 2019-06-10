4b4e628333985fc861db7a202033139fb900ff54
Jays-Code <dfbfbc@gmail.com>
2019-06-10 10:06:57 -0400

    working on models

--- AlphaKnowledge_app/models.py
+++ AlphaKnowledge_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    #conversations = models.CharField(max_length=400)
+    #topics= models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+class Conversations(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+    #time = 
+    username = models.ForeignKey(User)
+
+class Topics(models.Model):
+    subject = models.CharField(max_length=255)
+    notes = models.CharField(max_length=400)
+    username = models.ForeignKey(User)
---
+++ client/src/components/UserLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UserLogin extends React.Component {
+    render() {
+        return (
+            <p>Login Component</p>
+        )
+    }
+    
+}
+
+export default UserLogin;
