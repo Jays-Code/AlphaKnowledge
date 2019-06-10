8a0c8c5bdecb57a553a369cd491179cddb604bbd
Jays-Code <dfbfbc@gmail.com>
2019-06-10 15:52:16 -0400

    setup views and urls in app

--- AlphaKnowledge_app/urls.py
+++ AlphaKnowledge_app/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('user', views.UserView)
+router.register('conversations', views.ConversationsView)
+router.register('topics', views.TopicsView)
+
+
 urlpatterns = [
+path ('', include(router.urls))
 ]
--- AlphaKnowledge_app/views.py
+++ AlphaKnowledge_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer, ConversationsSerializer, TopicsSerializer
+from .models import User, Conversations, Topics
 
 # Create your views here.
+
+
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ConversationsView(viewsets.ModelViewSet):
+    queryset = Conversations.objects.all()
+    serializer_class = ConversationsSerializer
+
+class TopicsView(viewsets.ModelViewSet):
+    queryset = Topics.objects.all()
+    serializer_class = TopicsSerializer
