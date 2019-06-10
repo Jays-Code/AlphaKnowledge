571b914b9aa576b1386de45d7c3c18fce6a7daf6
Jays-Code <dfbfbc@gmail.com>
2019-06-10 15:35:04 -0400

    created serializer file

---
+++ AlphaKnowledge_app/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import User, Conversations, Topics
+
+
+class UserSerializer(serializers.ModelSerializer):
+    Conversations = ConversationsSerializer(many=True, read_only=True)
+    Topics = TopicsSerializer(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+class ConversationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conversations
+        fields = ('question', 'answer', 'time', 'username')
+
+class TopicsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topics
+        fields = ('subject', 'notes', 'username')
