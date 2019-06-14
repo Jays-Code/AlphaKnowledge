97179d174b9cb815571acf8f108a14f66d45e912
Jays-Code <dfbfbc@gmail.com>
2019-06-14 12:21:45 -0400

    reformats UserLogin

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -30,70 +30,70 @@ class UserLogin extends React.Component {
 
     // }
     // set state of redirect to true with --  .then(() => this.setState({ redirect: true}))
-/*
-    getUserList = () => {
-        axios.get('api/v1/user').then(res => {
-            this.setState({ users: res.data })
-        })
-    }
-
-    componentDidMount = () => {
-        this.getUserList()
-    }
-
-    createUser = (event) => {
-        event.preventDefault()
-        axios.post('api/v1/user/', {
-            username: this.state.newUser.username,
-            password: this.state.newUser.password
-        }).then(res => {
-            const usersList = [...this.state.users]
-            usersList.unshift(res.data)
-            this.setState({
-                newUser: {
-                    name: '',
-                    password: ''
-                },
-                users: usersList
+    /*
+        getUserList = () => {
+            axios.get('api/v1/user').then(res => {
+                this.setState({ users: res.data })
             })
-
-        })
-
-    }
-*/
+        }
+    
+        componentDidMount = () => {
+            this.getUserList()
+        }
+    
+        createUser = (event) => {
+            event.preventDefault()
+            axios.post('api/v1/user/', {
+                username: this.state.newUser.username,
+                password: this.state.newUser.password
+            }).then(res => {
+                const usersList = [...this.state.users]
+                usersList.unshift(res.data)
+                this.setState({
+                    newUser: {
+                        name: '',
+                        password: ''
+                    },
+                    users: usersList
+                })
+    
+            })
+    
+        }
+    */
     render() {
 
         if (this.state.redirect) {
             return <Redirect to='/userdashboard/:id' />;
         }
         return (
-<div>
             <div>
-                <h2>Login with a user</h2>
-                {this.state.users.map((user, index) => {
-                    return (
-                        <div key={index}>
-                            <Link to={{pathname: `userdashboard/${user.username}`, user: user}}>{user.username}</Link>
-                        </div>
-                    )
-                })
+                <div>
+                    <h2>Login with a user</h2>
+                    {this.state.users.map((user, index) => {
+                        return (
+                            <div key={index}>
+                                <Link to={{ pathname: `userdashboard/${user.username}`, user: user }}>{user.username}</Link>
+                            </div>
+                        )
+                    })
 
-                }
-            
-           
-            <form onSubmit={this.createUser}>
-                <div>
-                    <StyledLabel htmlFor="username">Username: </StyledLabel>
-                    <input
-                        id="username"
-                        type="text"
-                        name="username"
-                        onChange={this.handleChange}
-                        placeholder={this.state.loginInfo.username}
-                    />
-                </div>
-                <div>
-                    {/*
+                    }
+
+
+                    <form onSubmit={this.createUser}>
+                        <div>
+                            <StyledLabel htmlFor="username">Username: </StyledLabel>
+                            <input
+                                id="username"
+                                type="text"
+                                name="username"
+                                onChange={this.handleChange}
+                                placeholder={this.state.loginInfo.username}
+                            />
+                        </div>
+                        <div>
+                            {/*
                     <StyledLabel htmlFor="password">Password: </StyledLabel>
 
                     <input
@@ -104,43 +104,43 @@ class UserLogin extends React.Component {
                         placeholder={this.state.loginInfo.password}
                     />
                      */}
-                    <button type="submit">Login</button>
-                   
+                            <button type="submit">Login</button>
 
+
+                        </div>
+                    </form>
                 </div>
-            </form>
+
+
+
+
+
+                <div>
+                    <h2>Create a new user</h2>
+                    <form onSubmit={this.proceedLogin} >
+                        <StyledLabel htmlFor="subject">Username: </StyledLabel>
+                        <input
+                            id="username"
+                            input type="text"
+                            name="username"
+                            onChange={this.handlechange}
+                            placeholder={this.state.newUser.username}
+                        />
+                        <br></br>
+
+                        <StyledLabel>Password: </StyledLabel>
+                        <input
+                            id="password"
+                            input type="text"
+                            name="password"
+                            onChange={this.handleChange}
+                            placeholder={this.state.newUser.password}
+                        />
+
+                        <button type="submit">Create</button>
+                    </form>
+                </div>
             </div>
-
-
-
-
-            
-                        <div>
-                            <h2>Create a new user</h2>
-                            <form onSubmit={this.proceedLogin} >
-                                <StyledLabel htmlFor="subject">Username: </StyledLabel>
-                                <input
-                                    id="username"
-                                    input type="text"
-                                    name="username"
-                                    onChange={this.handlechange}
-                                    placeholder={this.state.newUser.username}
-                                />
-                                <br></br>
-                             
-                                <StyledLabel>Password: </StyledLabel>
-                                <input
-                                    id="password"
-                                    input type="text"
-                                    name="password"
-                                    onChange={this.handleChange}
-                                    placeholder={this.state.newUser.password}
-                                />
-                    
-                                <button type="submit">Create</button>
-                            </form>
-                        </div>
-                        </div>
         )
     }
 
